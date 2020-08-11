0016b65e308b529566f9b5a891b34a4e148690d2
Nicole Stellatos <stellatosnicole@gmail.com>
2020-08-11 02:02:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,28 +1,44 @@
-class Triangle 
-  attr_reader :size_1, :size_2, :size_3
-  def initialize(size_1,size_2, size_3)
-  @size_1 = size_1
-  @size_2 = size_2
-  @size_3 = size_3
-end
-def kind()
-if size_1 == size_2 && size_2 == size_3
-    :equilateral
-  elsif 
-  size_1 == size_2 || size_2 == size_3 || size_1 == size_3
-  :isosceles
-else
-  :scalene
-end
- end
- def valid?
-  valid_triangle = [(size_1 + size_2 > size_3), (size_1 + size_3 > size_2), (size_2 + size_3 > size_1)]
-  [size_1,size_2,size_3].each {|s| valid_triangle << false if s <= 0 }
-    raise TriangleError if valid_triangle.include?(false)
-  end
 
-class TriangleError < StandardError
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Triangle 
+#   attr_reader :size_1, :size_2, :size_3
+#   def initialize(size_1,size_2, size_3)
+#   @size_1 = size_1
+#   @size_2 = size_2
+#   @size_3 = size_3
+# end
+# def kind()
+# if size_1 == size_2 && size_2 == size_3
+#     :equilateral
+#   elsif 
+#   size_1 == size_2 || size_2 == size_3 || size_1 == size_3
+#   :isosceles
+# else
+#   :scalene
+# end
+# end
+# def valid?
+#   valid_triangle = [(size_1 + size_2 > size_3), (size_1 + size_3 > size_2), (size_2 + size_3 > size_1)]
+#   [size_1,size_2,size_3].each {|s| valid_triangle << false if s <= 0 }
+#     raise TriangleError if valid_triangle.include?(false)
+#   end
+
+# class TriangleError < StandardError
   
   
-end
-end
+# end
+# end
